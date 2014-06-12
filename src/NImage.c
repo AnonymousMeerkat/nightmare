083bf37b699e1b96a6e7a5b0244668e74a3df32f
Anonymous Meerkat <meerkatanonymous@gmail.com>
2014-06-11 19:03:45 -0600

    Replaced FreeImage by LodePNG

--- src/NImage.c
+++ src/NImage.c
@@ -7,7 +7,7 @@
 #include "NTypes.h"
 
 #include "wrap/gl.h"
-#include <FreeImage.h>
+#include <lodepng.h>
 #include <stdlib.h>
 #include <GLKit/GLKMath.h>
 
@@ -28,10 +28,6 @@ bool NImage_new_fbo(NImage* image) {
     image->size = N_win_size;
     glGenFramebuffers(1, &data->fbo);
     glBindFramebuffer(GL_FRAMEBUFFER, data->fbo);
-    /*glGenRenderbuffers(1, &data->rbo);
-    glBindRenderbuffer(GL_RENDERBUFFER, data->rbo);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA8, N_win_size.x, N_win_size.y);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, data->rbo);*/
     glBindTexture(GL_TEXTURE_2D, image->id);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, N_win_size.x, N_win_size.y, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -51,29 +47,6 @@ bool NImage_new_fbo(NImage* image) {
     glClear(GL_COLOR_BUFFER_BIT);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     return true;
-    /*// Basically copied over from DBAB
-    glActiveTexture(GL_TEXTURE0);
-    NImage_bind(image);
-    NImage_gl_parameters();
-    fbo_data* data = malloc(sizeof(fbo_data));
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, N_win_size.x, N_win_size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glGenRenderbuffers(1, &data->rbo);
-    glBindRenderbuffer(GL_RENDERBUFFER, data->rbo);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, N_win_size.x, N_win_size.y);
-    glBindRenderbuffer(GL_RENDERBUFFER, 0);
-    glGenFramebuffers(1, &data->fbo);
-    glBindFramebuffer(GL_FRAMEBUFFER, data->fbo);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, image->id, 0);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, data->rbo);
-    image->data = data;
-    GLenum status;
-    if ((status = glCheckFramebufferStatus(GL_FRAMEBUFFER)) != GL_FRAMEBUFFER_COMPLETE) {
-        Nerror("Framebuffer error! %i", status);
-        return false;
-    }
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    return true;*/
 }
 
 void NImage_destroy_fbo(NImage* image) {
@@ -122,45 +95,40 @@ bool NImage_load(NImage* image, char* path) {
         goto end;
     }
 
-    FREE_IMAGE_FORMAT fif = FreeImage_GetFileType(path, 0);
-    if (fif == FIF_UNKNOWN) {
-        fif = FreeImage_GetFIFFromFilename(path);
-    }
+    unsigned int error;
+    unsigned char* raw_data;
+    unsigned int width, height;
 
-    if (fif != FIF_UNKNOWN && FreeImage_FIFSupportsReading(fif)) {
-        image->data = FreeImage_Load(fif, path, 0);
-        if (!image->data) {
-            Nerror("Error loading image!");
-            goto end;
-        }
-
-        FIBITMAP* temp = image->data;
-        image->data = FreeImage_ConvertTo32Bits(temp);
-        FreeImage_Unload(temp);
-
-        if (!image->data) {
-            Nerror("Error converting image!");
-            goto end;
-        }
-
-        image->size.x = FreeImage_GetWidth(image->data);
-        image->size.y = FreeImage_GetHeight(image->data);
-
-        NImage_bind(image);
-        NImage_gl_parameters();
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image->size.x, image->size.y, 0,
-            GL_BGRA, GL_UNSIGNED_BYTE, FreeImage_GetBits(image->data));
-        glGenerateMipmap(GL_TEXTURE_2D);
-        NImage_unbind();
-
-        ret = true;
-    } else {
-        Nerror("Image %s not supported!", path);
+    error = lodepng_decode32_file(&raw_data, &width, &height, path);
+    image->size.x = width;
+    image->size.y = height;
+    if (error) {
+        Nerror("Error loading image %s (%u): %s", path, error, lodepng_error_text(error));
+        ret = false;
         goto end;
     }
 
-    FreeImage_Unload(image->data);
-    image->data = NULL;
+    unsigned char* good_data = malloc(width * height * 4);
+    for (size_t y = 0; y < height; y++) {
+        for (size_t x = 0; x < width; x++) {
+            for (size_t c = 0; c < 4; c++) {
+                good_data[4 * width * y + 4 * x + c] = raw_data[4 * width * (height - y) + 4 * x + c];
+            }
+        }
+    }
+
+    free(raw_data);
+
+    NImage_bind(image);
+    NImage_gl_parameters();
+    glTexImage2D(GL_TEXTURE_2D, 0, 4, image->size.x, image->size.y, 0,
+        GL_RGBA, GL_UNSIGNED_BYTE, good_data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    NImage_unbind();
+
+    free(good_data);
+
+    ret = true;
 end:
     return ret;
 }
