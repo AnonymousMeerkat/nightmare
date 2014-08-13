39eaa9527c1d0fa98dee9c02bd1d5ab1d7d446c8
Anonymous Meerkat <meerkatanonymous@gmail.com>
2014-08-12 18:47:31 -0600

    Added extension only for desktop

--- src/NShader.c
+++ src/NShader.c
@@ -36,7 +36,7 @@
 #ifdef USE_EGL
 const char versionstr[] = "#version 100\n";
 #else
-const char versionstr[] = "#version 120\n";
+const char versionstr[] = "#version 120\n#extension GL_EXT_gpu_shader4 : enable\n";
 #endif
 
 void NShader_print_log(bool shader, GLuint handle, char* prefix) {
